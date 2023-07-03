4331f3957a49a8702c0edd8ea0324d7468e33eca
Facundo <wehrlifacundo9@gmail.com>
2023-07-03 20:36:01 -0300

    main

---
+++ OOP/Ejercicio Integrador Plantas/src/main/java/org/practise/Main.java
@@ -0,0 +1,16 @@
+package org.practise;
+
+public class Main {
+    public static void main(String[] args) {
+        Arbol arbolito = new Arbol();
+        Arbusto arbustito = new Arbusto();
+        Flor florcita = new Flor();
+
+        arbolito.saludoPlanta();
+        arbustito.saludoPlanta();
+        florcita.saludoPlanta();
+
+    }
+
+
+}
