8b6c9a18ab26f13e770dea21ceb8d0b71a1bcb6e
Facundo <wehrlifacundo9@gmail.com>
2023-08-26 11:25:05 -0300

    ignore

---
+++ OOP/Peluqueria Canina/src/main/java/org/perritos/Main.java
@@ -0,0 +1,7 @@
+package org.perritos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ OOP/Peluqueria Canina/src/main/java/org/perritos/gui/Principal.java
@@ -0,0 +1,9 @@
+package org.perritos.gui;
+
+import javax.swing.*;
+
+public class Principal {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
